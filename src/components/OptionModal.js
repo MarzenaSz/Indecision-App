60cc08d6d44a18263353c71953def60b78649ea5
Marzena <marzena_szopinska@hotmail.com>
2018-01-13 23:18:10 +0000

    added code to the Modal that displays value to the screen

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,6 +5,7 @@ import Modal from 'react-modal';
 const OptionModal = (props) => (
         <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option">
             <h3>Selected Option</h3>
+            {props.selectedOption && <p>{props.selectedOption}</p>}
         </Modal>
 );
 
