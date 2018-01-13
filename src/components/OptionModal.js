fb2b28892a4e6fe927bf069e71a79b04b615d963
Marzena <marzena_szopinska@hotmail.com>
2018-01-13 23:49:42 +0000

    added onRequestClose handler to close the Modal by means of ESC and clicking on a background

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,7 +3,7 @@ import Modal from 'react-modal';
 
 // use implicit return
 const OptionModal = (props) => (
-        <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option">
+        <Modal isOpen={!!props.selectedOption} onRequestClose={props.handleClearSelectedOption} contentLabel="Selected Option">
             <h3>Selected Option</h3>
             {props.selectedOption && <p>{props.selectedOption}</p>}
             <button onClick={props.handleClearSelectedOption}>Okay</button>
