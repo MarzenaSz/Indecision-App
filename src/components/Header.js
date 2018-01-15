4d2768c99880e6668e4c7eb498f3b967e3212de5
Marzena <marzena_szopinska@hotmail.com>
2018-01-15 15:26:34 +0000

    styling the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Header = (props) => (
-    <div>
+    <div className="header">
         {/* use the data (props) */}
-        <h1>{props.title}</h1>
+        <h1 className='header__title'>{props.title}</h1>
         {/* show the subtitle only when a title is provided */}
-        {props.title && <h2>{props.subtitle}</h2>}
+        {props.title && <h2 className='header__subtitle'>{props.subtitle}</h2>}
     </div>
 );
 
