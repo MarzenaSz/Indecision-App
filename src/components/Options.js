e13bfaa4c5c82ee7c8e5107cb8b07df1daa26fd5
Marzena <marzena_szopinska@hotmail.com>
2018-01-17 23:31:34 +0000

    stylized remove buttons

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,7 +4,7 @@ import { debug } from 'util';
 
 const Options = (props) => (
     <div>
-        <button onClick={props.handleDeleteOptions}>Remove All</button>
+        <button className="button button--link" onClick={props.handleDeleteOptions}>Remove All</button>
         {/* show the message when there are no options */}
         {props.options.length === 0 && <p>Please add an option to get started</p>}
         {/* create new instance of Option for each item in an array */}
